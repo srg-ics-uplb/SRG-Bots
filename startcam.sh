59c6d5fde0de42c3d585b7082b87f51cba05ee57
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-01 12:33:11 +0800

    edits

--- startcam.sh
+++ startcam.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-
-# by jachermocilla@gmail.com
-#
-# Start recording videos using the 
-# USB Webcam payload using avconv.
-#
-# Uncomment the option to use
-#
-
-
 FILENAME=srgbotcam-`date +"%Y%m%d_%H%M%S"`.avi
 DURATION=300
-VIEWER_IP=10.0.0.2
-VIEWER_PORT=1234
 
-#Option 1) record and save video
+
+#record video
 #sudo avconv -f video4linux2 -i /dev/video0 -s vga -vf transpose=1,transpose=1 -t $DURATION $FILENAME
 
-#Option 2) record and save audio and video
 #sudo avconv -f video4linux2 -r 25 -i /dev/video0 -f alsa -i plughw:C525,0 -ar 22050 -ab 64k -strict experimental -async 1 -acodec aac -vcodec mpeg4 -y webcam.mp4
 
-#Option 3) UDP streaming 
-#To view: $vlc udp://@0.0.0.0:1234
-sudo avconv -f video4linux2 -i /dev/video0 -s qqvga -vf transpose=1,transpose=1 -c:v mpeg4 -f mpegts udp://$VIEWER_IP:$VIEWER_PORT
+#streaming and recording
+#To view: $vlc rtp://0.0.0.0:1234
+sudo avconv -f video4linux2 -s 160x120 -i /dev/video0 -vcodec mpeg2video -r 25 -vf transpose=1,transpose=1 -pix_fmt yuv420p -me_method epzs -b 2600k -bt 256k -f rtp rtp://10.0.0.3:1234
