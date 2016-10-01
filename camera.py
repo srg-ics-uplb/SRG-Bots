ebb2f3d65bf40512ab9acf9185de41893080d35f
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-01 12:46:01 +0800

    saves video

--- camera.py
+++ camera.py
@@ -3,19 +3,23 @@ import cv2
 
 cap = cv2.VideoCapture("rtp://0.0.0.0:1234")
 
+# Define the codec and create VideoWriter object
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (160,120))
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out.write(frame);
 
-    # Display the resulting frame
-    #cv2.imshow('frame',gray)
     cv2.imshow('frame',frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
+
         break
 
 # When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
