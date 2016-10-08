a75c0f77e287bc37683502cfc993613d78b46cf8
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-08 21:43:52 +0800

    edits

--- alt.py
+++ alt.py
@@ -4,6 +4,10 @@ connection_string = "udp:127.0.0.1:14551"
 from dronekit import connect, VehicleMode
 import time
 
+def meters_to_feet(meters):
+    return meters * 3.2808399
+
+
 # Connect to the Vehicle.
 print("Connecting to vehicle on: %s" % (connection_string,))
 vehicle = connect(connection_string, wait_ready=True)
@@ -19,6 +23,12 @@ print " Mode: %s" % vehicle.mode.name    # settable
 
 while True:
     print " Altitude: ", vehicle.location.global_relative_frame.alt 
+    feet=meters_to_feet(vehicle.location.global_relative_frame.alt)
+    inches = (feet * 12) % 12
+    feet = int(feet)
+
+    print "meters is", feet, "feet", inches, "inches"
+
     time.sleep(1)
 
 # Close vehicle object before exiting script
