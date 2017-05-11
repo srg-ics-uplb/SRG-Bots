cd4fe319d0dfa9cb1eedcbb771b320fe666b04aa
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-05-11 14:31:50 +0800

    added entry if there is no tablet

--- start-mavproxy.sh
+++ start-mavproxy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#for use if there is no tablet
+mavproxy.py --master udp:10.0.0.2:6000 --out 127.0.0.1:14550 --out 127.0.0.1:14551
+#for use if there is a tablet
 mavproxy.py --master udp:10.0.0.2:6000 --out 10.0.0.3:14550 --out 127.0.0.1:14551
