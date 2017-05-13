0eed25ed190a84612d4b589b1e63e2ae7bc05f86
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-05-13 13:12:31 +0800

    edits

--- srgbot-log-download.sh
+++ srgbot-log-download.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-LOGROOT=.
-LOGDIR=srgbot-log-`date +%Y%m%d-%H%M%S`
+CURRENT=`pwd`
+LOGROOT=../dataflashlogs
+LOGDIR=$LOGROOT/srgbot-log-`date +%Y%m%d-%H%M%S`
 echo $LOGDIR
 
-mkdir log-$DATE
+mkdir $LOGDIR
 cd $LOGDIR
 scp -p erle@10.0.0.1:./APM/logs/* .
-ssh erle@10.0.0.1 rm ./APM/logs/*
-cd ..
+ssh -t erle@10.0.0.1 sudo rm ./APM/logs/*
+cd $CURRENT
